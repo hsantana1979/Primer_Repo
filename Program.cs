881a72982898e8611f0b9b36ac7fc8fe46d98a11
hsantana1979 <hensatel@gmail.com>
2025-09-20 22:35:19 -0600

    Agregando Proyecto sin archivo temporales de VS

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+Console.WriteLine("Digite su Nombre");
+string nombreUsuario = Console.ReadLine();
+
+Console.WriteLine("Digite su Edad");
+string edadUsuario = Console.ReadLine();
+
+int edad = int.Parse(edadUsuario);
+
+Saludar(nombreUsuario);
+
+if (edad >= 18)
+{
+    Console.WriteLine("Eres mayor de edad");
+}
+else
+{
+    Console.WriteLine("Eres menor de edad");
+}
+
+static void Saludar(string nombre)
+{
+    Console.WriteLine("Hola "+ nombre);
+}
